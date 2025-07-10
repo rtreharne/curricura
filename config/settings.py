1d5b436429491258827edffeab3af29f91b28f37
Robert Treharne <treharne@liverpool.ac.uk>
2025-07-10 16:40:58 +0100

    configuring redis and celery. important for later (probably)

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CELERY
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+
